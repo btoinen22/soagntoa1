bdb7f5d89ec74d65e7a8604a77c6325176aacdbb
benoit toinen <benoit.toinen@yahoo.com>
2021-09-21 16:55:11 +0200

    Ajoutez des fichiers projet.

---
+++ soagntoa1/soagntoa1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using soagntoa1.Services;
+using soagntoa1.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace soagntoa1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ soagntoa1/soagntoa1/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using soagntoa1.ViewModels;
+using soagntoa1.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace soagntoa1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
