47a0d2f9f707f8234ad5830a09d75f1ddde240ca
jazouben <benoit.toinen@yahoo.com>
2021-09-28 13:56:56 +0200

    add "using notes; " suppression de l'erreur d'instance App

--- soagntoa1/soagntoa1.Android/MainActivity.cs
+++ soagntoa1/soagntoa1.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Notes;
 
 namespace soagntoa1.Droid
 {
@@ -16,7 +17,7 @@ namespace soagntoa1.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new app());
+            LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
